d67c7b598f654658f9eec193c8161458c21aec7a
Matt Roberts <roberts.mattroberts@gmail.com>
2024-04-22 15:37:56 +0100

    I am an idiot

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https//mattwoberts.github.io',
+  site: 'https://mattwoberts.github.io',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
